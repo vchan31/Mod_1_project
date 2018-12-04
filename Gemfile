c2671af67ab2c2a331148313e536519274373db8
Vincent Chan <vchan313@gmail.com>
2018-12-04 10:55:33 -0500

    created folders and base files for app

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+gem 'pry'
+gem 'sinatra-activerecord'
+gem 'sqlite3'
+gem 'rake'
+gem 'database_cleaner'
---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'bundle/setup'
+
+require 'sinatra-activerecord'
+
+Bundler.require
+
+# code to join database to ruby .... tbd , check old labs.
+
